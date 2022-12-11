608af29be544bd281e4caed0b3ba9d8de065b0c0
dyalnpahm <117327438+dyalnpahm@users.noreply.github.com>
2022-12-11 14:22:40 +1100

    HTML

---
+++ JS/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+  numb: 1,
+  question: "Arrays in JavaScript are defined by which of the following statements?",
+  answer: "It is an ordered list of values",
+  options: [
+    " It is an ordered list of values",
+    "It is an ordered list of objects",
+    "It is an ordered list of string",
+    "It is an ordered list of functions"
+  ]
+},
+  {
+  numb: 2,
+  question: "Which of the following is not a javascript data type?",
+  answer: "Array",
+  options: [
+    "Undefined",
+    "Number",
+    "Array",
+    "Boolean"
+  ]
+},
+  {
+  numb: 3,
+  question: "What is Javascript?",
+  answer: "JavaScript is a scripting language used to make the website interactive",
+  options: [
+    "JavaScript is a scripting language used to make the website interactive",
+    "JavaScript is an assembly language used to make the website interactive",
+    "JavaScript is a compiled language used to make the website interactive",
+    "None of the mentioned"
+  ]
+},
+  {
+  numb: 4,
+  question: "Which of the following is correct about JavaScript?",
+  answer: "JavaScript is an Object-Based language",
+  options: [
+    "JavaScript is Assembly-language",
+    "JavaScript is an Object-Oriented language",
+    "JavaScript is a High-level language",
+    "JavaScript is an Object-Based language"
+  ]
+},
+  {
+  numb: 5,
+  question: "Which of the following keyword is used to define a variable in javascript?",
+  answer: "Var",
+  options: [
+    "function",
+    "if",
+    "for",
+    "var"
+  ]
+},
+// you can uncomment the below codes and make duplicate as more as you want to add question
+// but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+//   {
+//   numb: 6,
+//   question: "Your Question is Here",
+//   answer: "Correct answer of the question is here",
+//   options: [
+//     "Option 1",
+//     "option 2",
+//     "option 3",
+//     "option 4"
+//   ]
+// },
+];
---
+++ JS/script.js
@@ -0,0 +1,11 @@
+const start_btn = document.querySelector(".start_btn button");
+const quiz_box = document.querySelector(".quiz_box");
+const result_box = document.querySelector(".result_box");
+const option_list = document.querySelector(".option_list");
+const time_line = document.querySelector("header .time_line");
+const timeText = document.querySelector(".timer .time_left_txt");
+const timeCount = document.querySelector(".timer .timer_sec");
+
+start_btn.onclick = ()=>{
+quiz_box.classList.add("activeQuiz");   
+}
